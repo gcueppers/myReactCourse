0b59ef6036a277e160e2adfacc33fece288a0270
Guido Majewski <guido.majewski@t-systems.com>
2015-10-16 14:46:38 +0200

    section 5 of the course

--- 05_thumbnail-gulp/src/app.jsx
+++ 05_thumbnail-gulp/src/app.jsx
@@ -1,13 +1,16 @@
+var React = require('react');
+var ThumbnailList = require('./thumbnail-list');
+
 var options = {
     thumbnailData: [{
         title: 'Show Courses',
-        number: 12,
+        number: 43,
         header: 'Learn React',
         description: 'React is a fantastic new front end library for rendering web pages. React is a fantastic new front end library for rendering web pages.',
         imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
     },{
         title: 'Show Courses',
-        number: 25,
+        number: 120,
         header: 'Learn Gulp',
         description: 'Gulp will speed up your development workflow.  Gulp will speed up your development workflow.  Gulp will speed up your development workflow.',
         imageUrl: 'http://brunch.io/images/others/gulp.png'
